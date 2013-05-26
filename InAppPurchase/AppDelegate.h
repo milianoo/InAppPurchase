3149d6204ca0309a78ef71ab6deb1f202902b894
Milad Rezazadeh <miladrezazadeh@Milads-MacBook-Pro.local>
2013-05-26 19:38:46 +0800

    Initial Commit

---
+++ InAppPurchase/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  InAppPurchase
+//
+//  Created by Milad Rezazadeh on 5/26/13.
+//  Copyright (c) 2013 mindvalley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ InAppPurchase/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InAppPurchase
+//
+//  Created by Milad Rezazadeh on 5/26/13.
+//  Copyright (c) 2013 mindvalley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
